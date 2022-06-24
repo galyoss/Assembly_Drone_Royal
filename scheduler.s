3ce7cba96793f65a5d5c63dfa04b9bcb1722ded1
Gal <galyossipov@gmail.com>
2022-06-24 14:39:27 +0300

    alias

--- scheduler.s
+++ scheduler.s
@@ -18,6 +18,40 @@
 %endmacro
 
 section	.rodata
+string_format: db "%s", 10, 0			
+	decimal_format: db "%d", 0  			
+	float_format: db "%.2f", 0 			    ;2digit precision
+    new_line_format: db 10, 0
+    comma_format: db ",", 0
+    DroneStructLen: equ 37 ; 8xpox, 8ypos, 8angle, 8speed, 4kills, 1isActive
+    DRONE_STRUCT_XPOS_OFFSET: equ 0
+    DRONE_STRUCT_YPOS_OFFSET: equ 8
+    DRONE_STRUCT_HEADING_OFFSET: equ 16
+    DRONE_STRUCT_SPEED_OFFSET: equ 24
+    DRONE_STRUCT_KILLS_OFFSET: equ 32
+    DRONE_STRUCT_ACTIVE_OFFSET: equ 36
+    DRONE_STRUCT_KILLS:
+    TARGET_STRUCT_SIZE: equ 17
+    TARGET_STRUCT_XPOS_OFFSET: equ 0
+    TARGET_STRUCT_YPOS_OFFSET: equ 8
+    TARGET_STRUCT_IS_DESTROYED_OFFSET: equ 16
+    MAX_DEGREE: equ 360
+    CO_STK_SIZE: equ 16384 ; 16*1024
+    BIT_MASK_16: equ 1
+    BIT_MASK_14: equ 4
+    BIT_MASK_13: equ 8
+    BIT_MASK_11: equ 32
+    MAX_SEED: equ 65535
+    MAX_SPEED: equ 50
+    MAX_ANGLE_DELTA_LIM: equ 60
+    MIN_ANGLE_DELTA_LIM: equ -60
+    BOARD_SIZE: equ 100
+    format_d:   db "%d", 0
+    format_f:   db "%f", 0
+    MAX_DELTA_DEG_RANGE: equ 120
+    MAX_DELTA_POS_RANGE: equ 10
+    scaled_rnd_format: db "Scaled rnd with limit of %d, resuly is %d", 10, 0
+
 
 section .data
     curr_step: dd 0
@@ -37,11 +71,7 @@ section .text
     extern DronesArrayPointer
     extern DRONE_STRUCT_ACTIVE
     extern DronesArrayPointer
-    extern DRONE_STRUCT_ACTIVE_OFFSET
-    extern DRONE_STRUCT_KILLS_OFFSET
-    extern DRONE_STRUCT_KILLS_OFFSET
     extern DronesArrayPointer
-    extern DRONE_STRUCT_ACTIVE_OFFSET
     global run_schedueler
 
 
