35f3d0e7c2068ec45d0751ee6ff654e1b429a7f8
Gal <galyossipov@gmail.com>
2022-06-24 14:38:01 +0300

    alias

--- printer.s
+++ printer.s
@@ -103,14 +103,6 @@ section .text
     extern printf
     extern target_pointer
     extern DronesArrayPointer
-    extern DRONE_STRUCT_ACTIVE_OFFSET
-    extern DRONE_STRUCT_XPOS_OFFSET
-    extern DRONE_STRUCT_YPOS_OFFSET
-    extern DRONE_STRUCT_SPEED_OFFSET
-    extern DRONE_STRUCT_HEADING_OFFSET
-    extern DRONE_STRUCT_KILLS_OFFSET
-    extern TARGET_STRUCT_YPOS_OFFSET
-    extern TARGET_STRUCT_XPOS_OFFSET
     extern cors
     extern resume
     global run_printer
