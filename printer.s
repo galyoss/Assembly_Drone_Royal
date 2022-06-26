e13b89d50f58791d1f5fa32f5ddb65d9d40275d9
Gal <galyossipov@gmail.com>
2022-06-26 10:45:03 +0300

    alias

--- printer.s
+++ printer.s
@@ -132,7 +132,7 @@ section .text
 
             ;format line: index (of drone), float(x), float(y), float(angle), float(speed), int(points)
             push dword [eax+DRONE_STRUCT_KILLS_OFFSET]
-            push dword [eax+DRONE_STRUCT_SPEED_OFFSET]
+            push byte [eax+DRONE_STRUCT_SPEED_OFFSET]
             push dword [eax+DRONE_STRUCT_SPEED_OFFSET+4]
             push dword [eax+DRONE_STRUCT_HEADING_OFFSET]
             push dword [eax+DRONE_STRUCT_HEADING_OFFSET+4]
